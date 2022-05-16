360b4729a426ba4f0fa0092ffdde44843256d41e
unknown <CITpc@CITnewPC.ck.local>
2022-05-16 16:47:39 +0300

    cache remove

--- bitrix/managed_cache/MYSQL/41/416acb540279140f55ddb5b83b7646b7.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001652702179';
-$dateexpire = '001652705779';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:2:"s1";a:4:{i:0;a:3:{s:9:"CONDITION";s:29:"CSite::InDir(\'/desktop_app/\')";s:8:"TEMPLATE";s:11:"desktop_app";s:7:"SITE_ID";s:2:"s1";}i:1;a:3:{s:9:"CONDITION";s:19:"$_GET[\'print\']==\'Y\'";s:8:"TEMPLATE";s:5:"print";s:7:"SITE_ID";s:2:"s1";}i:2;a:3:{s:9:"CONDITION";s:26:"CSite::InDir(\'/unseeing/\')";s:8:"TEMPLATE";s:7:"unseeng";s:7:"SITE_ID";s:2:"s1";}i:3;a:3:{s:9:"CONDITION";s:0:"";s:8:"TEMPLATE";s:4:"main";s:7:"SITE_ID";s:2:"s1";}}}}';
-return true;
-?>
--- bitrix/managed_cache/MYSQL/agents/10/10624e3811fc831f380b14e8fee934d9.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001652702178';
-$dateexpire = '001652705838';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1652705825;}';
-return true;
-?>
