8c75ab38b6838f2b2b319563786409c6939ae8da
unknown <CITpc@CITnewPC.ck.local>
2022-05-17 12:55:40 +0300

    task_1644

--- otdykh-v-belarusi/sanatorii-belarusi/searchresult.php
+++ otdykh-v-belarusi/sanatorii-belarusi/searchresult.php
@@ -39,6 +39,7 @@ $APPLICATION->SetTitle("Отдых в санаториях Беларуси 2021
                 numberPerPage: 20,
                 agent: "1094",
                 hash: "1c44b8a5c418ccd52725c222bd11c6c7",
+                lang: "ru",
                 mainIframeCss: ".btn-primary,.show-offers,.thumbnail{border-radius:0;font-size:13px}.show-offers{opacity:1;background:#EB5019;border-color:#EB5019;color:#FFF!important;padding:8px 12px 7px}.thumbnail{background:#FFF;-webkit-box-shadow:0 0 5px -1px rgba(0,0,0,.2);-moz-box-shadow:0 0 5px -1px rgba(0,0,0,.2);box-shadow:0 0 5px -1px rgba(0,0,0,.2);margin-bottom:30px;o-transition:all .3s ease-out;-ms-transition:all .3s ease-out;-moz-transition:all .3s ease-out;-webkit-transition:all .3s ease-out;line-height:22px;font-family:Arial,sans-serif;color:#3f3f3f}.glyphicon{width:20px;height:20px;line-height:20px;border-radius:50%;background:#EB5019;color:#FFF;text-align:center;font-size:10px}p{font-size:13px}.panel-title{font-size:22px;font-size:17px;color:#333}.panel-group .panel{border-radius:0}.panel-heading{border-top-left-radius:0;border-top-right-radius:0}.container{width: 100%;}"
             }
         });
@@ -59,6 +60,7 @@ Travelsoft.init({
 									citizen_price: "332",
 									agent: "1094",
 									hash: "1c44b8a5c418ccd52725c222bd11c6c7",
+                  lang: "ru",
 									mainIframeCss: ".btn-primary,.show-offers,.thumbnail{border-radius:0;font-size:13px}.show-offers{opacity:1;background:#EB5019;border-color:#EB5019;color:#FFF!important;padding:8px 12px 7px}.thumbnail{background:#FFF;-webkit-box-shadow:0 0 5px -1px rgba(0,0,0,.2);-moz-box-shadow:0 0 5px -1px rgba(0,0,0,.2);box-shadow:0 0 5px -1px rgba(0,0,0,.2);margin-bottom:30px;o-transition:all .3s ease-out;-ms-transition:all .3s ease-out;-moz-transition:all .3s ease-out;-webkit-transition:all .3s ease-out;line-height:22px;font-family:Arial,sans-serif;color:#3f3f3f}.glyphicon{width:20px;height:20px;line-height:20px;border-radius:50%;background:#EB5019;color:#FFF;text-align:center;font-size:10px}p{font-size:13px}.panel-title{font-size:22px;font-size:17px;color:#333}.panel-group .panel{border-radius:0}.panel-heading{border-top-left-radius:0;border-top-right-radius:0}.container{width: 100%;}"
 	
 								}
@@ -83,6 +85,7 @@ $("a[href='#tab3']").one("click", function () {
                 numberPerPage: 20,
                 agent: "1094",
                 hash: "1c44b8a5c418ccd52725c222bd11c6c7",
+                lang: "ru",
                 mainIframeCss: ".btn-primary,.show-offers,.thumbnail{border-radius:0;font-size:13px}.show-offers{opacity:1;background:#EB5019;border-color:#EB5019;color:#FFF!important;padding:8px 12px 7px}.thumbnail{background:#FFF;-webkit-box-shadow:0 0 5px -1px rgba(0,0,0,.2);-moz-box-shadow:0 0 5px -1px rgba(0,0,0,.2);box-shadow:0 0 5px -1px rgba(0,0,0,.2);margin-bottom:30px;o-transition:all .3s ease-out;-ms-transition:all .3s ease-out;-moz-transition:all .3s ease-out;-webkit-transition:all .3s ease-out;line-height:22px;font-family:Arial,sans-serif;color:#3f3f3f}.glyphicon{width:20px;height:20px;line-height:20px;border-radius:50%;background:#EB5019;color:#FFF;text-align:center;font-size:10px}p{font-size:13px}.panel-title{font-size:22px;font-size:17px;color:#333}.panel-group .panel{border-radius:0}.panel-heading{border-top-left-radius:0;border-top-right-radius:0}.container{width: 100%;}"
             }
         });
